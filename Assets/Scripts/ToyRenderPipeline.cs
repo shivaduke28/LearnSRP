506d8387a9b643bae9ad1d8242b5bb02b24fe7ca
shivaduke28 <shivaduke28@gmail.com>
2023-11-03 17:37:20 +0900

    toy rp

---
+++ Assets/Scripts/ToyRenderPipeline.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ToySRP
+{
+    public sealed class ToyRenderPipeline : RenderPipeline
+    {
+        readonly ToyRenderPipelineAsset asset;
+
+        public ToyRenderPipeline(ToyRenderPipelineAsset asset)
+        {
+            this.asset = asset;
+        }
+
+        // 毎フレーム呼ばれる
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            foreach (var camera in cameras)
+            {
+                // depthバッファをクリアする。
+                if (camera.clearFlags != CameraClearFlags.Nothing)
+                {
+                    var cmd = new CommandBuffer();
+                    cmd.ClearRenderTarget(true, false, default);
+                    context.ExecuteCommandBuffer(cmd);
+                    cmd.Release();
+                }
+
+                // カメラのカリングパラメータを取得する。
+                if (!camera.TryGetCullingParameters(out var cullingParameters)) continue;
+                // カリングを実行。
+                var cullingResults = context.Cull(ref cullingParameters);
+
+                // "LightMode"="ToyOpaque"のPassを使用する。
+                var shaderTag = new ShaderTagId("ToyOpaque");
+
+                // Cameraからソート設定を作る。自前で用意することもできるようになってそう。
+                var sortingSetting = new SortingSettings(camera);
+                // この辺よくわかってない。
+                var drawingSettings = new DrawingSettings(shaderTag, sortingSetting);
+                // この辺よくわかってない2。
+                var filteringSettings = FilteringSettings.defaultValue;
+
+                // カメラの行列を GlobalShaderProperty にセットする。
+                context.SetupCameraProperties(camera);
+
+                // draw
+                context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+                // opaqueの後にSkyboxをレンダリングする
+                if (camera.clearFlags == CameraClearFlags.Skybox && RenderSettings.skybox != null)
+                {
+                    context.DrawSkybox(camera);
+                }
+
+#if UNITY_EDITOR
+                // scene viewならギズモをレンダリングする。
+                if (camera.cameraType == CameraType.SceneView)
+                {
+                    context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+                }
+#endif
+
+                // 実行。
+                context.Submit();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ToyRenderPipelineAsset.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ToySRP
+{
+    [CreateAssetMenu(menuName = "Rendering/ToyRenderPipelineAsset")]
+    public class ToyRenderPipelineAsset : RenderPipelineAsset
+    {
+        [SerializeField] Color someColor;
+        public Color SomeColor => someColor;
+
+        // Assetに紐づいたRenderPipelineを返す。
+        // このアセットにパラメータをシリアライズして、初期化時に渡すことができそう。
+        protected override RenderPipeline CreatePipeline()
+        {
+            return new ToyRenderPipeline(this);
+        }
+    }
+}
